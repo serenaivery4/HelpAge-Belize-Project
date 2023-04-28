ef1c53392162009f2d21865c12bc7c2134dea6c4
jthunt1515 <jthunt1515@gmail.com>
2023-04-28 01:48:08 -0500

    restyle the patient source tab

--- react_belize/src/patientSources/PatientSources.js
+++ react_belize/src/patientSources/PatientSources.js
@@ -1,4 +1,6 @@
-import tropicalBanner from './images/tropicalBanner.jpeg'
+import tropicalBanner from './images/tropicalBanner2.png'
+import belize1 from './images/belize1.jpeg'
+import belize2 from './images/belize2.jpeg'
 
 import './patientSources.css';
 
@@ -11,15 +13,18 @@ function PatientSources() {
                 <img src={tropicalBanner} alt="Tropical Banner" className="tropicalBanner"/>
             </div>
         </div>
-        <div className="contentWrapper">        
-            <div className="content1">
-                content1
-            </div>
-            <div className="content2">
-                content2
-            </div>
+        <div className="contentWrapper"> 
+            <div className="left">
+                <div className="content1">
+                    content1
+                </div>
+                <div className="content2">
+                    content2
+                </div>  
+            </div>       
             <div className="content3">
-                content3
+                <img src={belize1} alt="Belize1" className="belize1"/>
+                <img src={belize2} alt="Belize2" className="belize2"/>
             </div>
         </div>
 
